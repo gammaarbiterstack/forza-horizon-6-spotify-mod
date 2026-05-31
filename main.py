bb2ff1918d96103f8634a18f491decbac9d0ec13
gammaarbiterstack <chastityimbrock@gmail.com>
2026-05-31 09:24:56 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+class SpotifyMod:
+    def __init__(self):
+        self.version = "1.0.0"
+
+    def start(self):
+        print("Forza Horizon 6 Spotify Mod started")
+
+    def connect(self):
+        print("Spotify connection initialized")
+
+if __name__ == "__main__":
+    app = SpotifyMod()
+    app.connect()
+    app.start()
